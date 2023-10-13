f944cce9f86025b465b44cadc11252b7d0adb4de
Mauricio Lauffer <443888+mauriciolauffer@users.noreply.github.com>
2023-10-13 16:28:08 +1000

    test: wdio-qunit-service

---
+++ webapp/test/wdio.conf.js
@@ -0,0 +1,20 @@
+exports.config = {
+	specs: ['**/*.test.js'],
+
+	capabilities: [{
+	  'browserName': 'chrome',
+	  'goog:chromeOptions': {
+		args: ['headless', 'disable-gpu']
+	  }
+	}],
+
+	logLevel: 'error',
+	framework: 'mocha',
+	reporters: ['spec'],
+
+	services: ['qunit'],
+	mochaOpts: {
+	  ui: 'bdd',
+	  timeout: 60000
+	}
+  };
